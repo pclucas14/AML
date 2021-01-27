431910f1e3588b0516b384e75718600ed691b882
pclucas14 <lucas.page-caccia@mail.mcgill.ca>
2021-01-27 14:29:21 +0000

    smol clean

--- model.py
+++ model.py
@@ -11,45 +11,16 @@ from torch.autograd import Variable
 from utils import Reshape
 
 
+# Custom Batch Norm layer
+# goal : control impact of each sample on the batch norm statistics
+
 def normalize(x):
     x_norm = torch.norm(x, p=2, dim=1).unsqueeze(1).expand_as(x)
     x_normalized = x.div(x_norm + 0.00001)
     return x_normalized
 
-class ContrastiveLoss(torch.nn.Module):
-    """
-    Contrastive loss function.
-    Based on:
-    """
 
-    def __init__(self, margin=1.0):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
-
-    def check_type_forward(self, in_types):
-        assert len(in_types) == 3
-
-        x0_type, x1_type, y_type = in_types
-        assert x0_type.size() == x1_type.shape
-        assert x1_type.size()[0] == y_type.shape[0]
-        assert x1_type.size()[0] > 0
-        assert x0_type.dim() == 2
-        assert x1_type.dim() == 2
-        assert y_type.dim() == 1
-
-    def forward(self, x0, x1, y):
-        self.check_type_forward((x0, x1, y))
-
-        # euclidian distance
-        diff = x0 - x1
-        dist_sq = torch.sum(torch.pow(diff, 2), 1)
-        dist = torch.sqrt(dist_sq)
-
-        mdist = self.margin - dist
-        dist = torch.clamp(mdist, min=0.0)
-        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
-        loss = torch.sum(loss) / 2.0 / x0.size()[0]
-        return loss
+# TODO: @Eugene please clean this up
 
 from torch.nn.utils.weight_norm import WeightNorm
 class distLinear(nn.Module):
@@ -201,3 +172,9 @@ class ResNet(nn.Module):
 def ResNet18(nclasses, nf=20, input_size=(3, 32, 32)):
     return ResNet(BasicBlock, [2, 2, 2, 2], nclasses, nf, input_size)
 
+if __name__ == '__main__':
+    x = torch.randn(10, 3, 100, 100)
+    mask = torch.FloatTensor(size=(10,)).uniform_(0,1)
+    mask = mask > 0.5
+    my_bn = MyBatchNorm2d(3, affine=True)
+    out = my_bn(x, stat_mask=mask)
