457b9348e70fdd84da26918e9bd968ef3d71e6ed
pclucas14 <lucas.page-caccia@mail.mcgill.ca>
2021-10-05 13:33:23 +0000

    cleanup

--- methods/__init__.py
+++ methods/__init__.py
@@ -4,7 +4,6 @@ from .er_aml import ER_AML_Triplet, ER_AML
 from .mir import MIR
 from .icarl import ICARL
 from .agem import AGEM, AGEMpp
-from .cope import CoPE
 from .der import DER, DERpp
 from .iid import IID, IIDpp
 from .ssil import SSIL
